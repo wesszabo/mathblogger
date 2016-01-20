c4a0fa52c332aef1717af83336b6374ba911a573
wesszabo <bmesszabo@gmail.com>
2016-01-20 19:19:54 +0100

    Create mathjaxConfig.js

---
+++ mathjaxConfig.js
@@ -0,0 +1,31 @@
+<script type="text/x-mathjax-config">
+  //
+  //  Make displayed equations be aligned left and indented.
+  //  Add the \Step macro and turn off noErrors so we can tell
+  //  what has gone wrong (if anything).
+  //
+ MathJax.Hub.Config({
+displayAlign: "left",
+    displayIndent: "2em",
+    TeX: {
+      Macros: {
+        Step: ["\\cssId{Step#1}{\\style{visibility:hidden}{{}#2}}",2]
+      },
+      noErrors: {disabled: true},
+	extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"],
+	equationNumbers: { autoNumber: "AMS" }
+    },
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    },
+    "HTML-CSS": { availableFonts: ["TeX"],linebreaks: {automatic: true} }
+  });
+  //
+  //  Enable the step buttons after the initial typesetting.
+  //
+  MathJax.Hub.Queue(["EnableSteps",DynamicEquation]);
+</script>
